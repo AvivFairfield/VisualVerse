471d8a486acb8ad7772caaeec3f8e2ee12cf46a4
Aviv Cohen <avivcohen22@icloud.com>
2024-04-08 03:11:52 +0300

    credits

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -38,7 +38,7 @@ import { updateCredits } from "@/lib/actions/user.actions";
 import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/actions/image.actions";
 import { useRouter } from "next/navigation";
-//import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
 	title: z.string(),
@@ -210,10 +210,9 @@ const TransformationForm = ({
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-				{
-					creditBalance < Math.abs(creditFee)
-					//<InsufficientCreditsModal />
-				}
+				{creditBalance < Math.abs(creditFee) && (
+					<InsufficientCreditsModal />
+				)}
 				<CustomField
 					control={form.control}
 					name="title"
